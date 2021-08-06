4cbb42130a00c9601a153d6b38520122cf314c9d
Maanush Putcha <p.maanush@gmail.com>
2021-08-06 19:41:54 +0530

    RMX1821: Add fingerprint hal for egis
    * Thanks to @dodyirawan85 and @sarthakroy2002 for the hal
    
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- RMX1821-vendor.mk
+++ RMX1821-vendor.mk
@@ -11,5 +11,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1821/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
     vendor/realme/RMX1821/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1821/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
+    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-rbs:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/bin/hw/android.hardware.biometrics.fingerprint@2.1-service-rbs \
     vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
     vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so
